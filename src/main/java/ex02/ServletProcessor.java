cd4b30e003e4617fb2a497043da53b633b5f5fa6
evanxwj <811690860@qq.com>
2019-05-09 19:43:27 +0800

    ex02

---
+++ src/main/java/ex02/ServletProcessor.java
@@ -0,0 +1,47 @@
+package ex02;
+
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandler;
+
+public class ServletProcessor {
+
+    public void process(Request request, Response response) {
+        String uri = request.getUri();
+        String servletName = uri.substring(uri.lastIndexOf("/") + 1);
+        URLClassLoader loader = null;
+        try {
+            // create a URLClassLoader
+            URL[] urls = new URL[1];
+            URLStreamHandler streamHandler = null;
+            File classPath = new File(Constants.WEB_ROOT);
+            String repository = new URL("file", null, classPath.getCanonicalPath() + File.separator).toString();
+            urls[0] = new URL(null, repository, streamHandler);
+            loader = new URLClassLoader(urls);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Class myClass = null;
+        try {
+            myClass = loader.loadClass(servletName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Servlet servlet = null;
+
+        try {
+            servlet = (Servlet) myClass.newInstance();
+            servlet.service(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
